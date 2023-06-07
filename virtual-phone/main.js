2c4727b47a13fca864a449c5cf6d9274711b4d5c
rasec-hsc <cesarhscn@hotmail.com>
2023-06-07 19:47:10 -0300

    aaa

--- virtual-phone/main.js
+++ virtual-phone/main.js
@@ -1,22 +1,27 @@
-function valorPrimeiroBotao(){
+function valorPrimeiroBotao() {
     alert(document.querySelector
         ('input[type=button]').value);
 }
 
 const listaTeclas = document.querySelectorAll
-('input[type=button]');
+    ('input[type=button]');
 const display = document.querySelector('input[type=tel]');
 
-function valorBotaoUmJanela(){
+function valorBotaoUmJanela() {
     alert(listaTeclas[0].value);
 }
 
-function valorBotaoUm(){
+function valorBotaoUm() {
     display.value += listaTeclas[0].value;
 }
 
-function valorBotao(valor){
-   display.value += listaTeclas[valor].value;
+function valorBotao(valor) {
+    if (display.value.length < 8 && listaTeclas[valor].value !== '#') {
+        display.value += listaTeclas[valor].value;
+    } else {
+        alert('Fora do limite permitido ou função não liberada.')
+    }
+
 }
 
 /* let i = 0;
@@ -36,7 +41,6 @@ for (let index = 0; index < listaTeclas.length; index++) {
     }
 }
 
-
-function limpar(){
+function limpar() {
     display.value = '';
 }
