e63d1a1d747b249e840e9b91dfe55b33d7623d54
rasec-hsc <cesarhscn@hotmail.com>
2023-06-07 20:22:34 -0300

    aa

---
+++ banner.js
@@ -0,0 +1,24 @@
+const banner = ["img/destaque-home-2.png", "img/destaque-home-2.png"];
+let bannerAtual = 1;
+
+function trocaBanner(){
+    bannerAtual = (bannerAtual + 1) %2;
+    document.querySelector(".destaque img").src = banners[bannerAtual];
+}
+
+setInterval(trocaBanner, 4000);
+
+
+let timer = setInterval(trocaBanner, 4000);
+const controle = document.querySelector(".pause");
+
+
+controle.addEventListener("click" , () =>{
+    if (controle.className === "pause") {
+        clearInterval(timer);
+        controle.className = "play";
+    } else {
+        timer = setInterval(trocaBanner, 4000);
+        controle.className = "pause";
+    }
+});
